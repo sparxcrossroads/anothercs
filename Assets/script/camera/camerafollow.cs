803c1ec8d0055981f00fe2d9626e4d154bd06e4d
sparxcrossroads <kisshotblade@sina.com>
2015-03-19 12:31:54 +0800

    第3人称l fps
    luck...=_,=

---
+++ Assets/script/camera/camerafollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerafollow : MonoBehaviour {
+    public float camera_height = 2.0f;
+    public float camera_distance = 4.0f;
+
+    private Transform player;
+    private Transform camera;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        camera = Camera.main.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //look at palyer
+        //camera.LookAt(player);
+
+        //forward
+        camera.eulerAngles = new Vector3(camera.eulerAngles.x,
+            player.eulerAngles.y,
+            camera.eulerAngles.z);
+
+        
+        float angle = camera.eulerAngles.y;
+
+        float deltax = camera_distance * Mathf.Sin(angle * Mathf.PI / 180);
+        float deltaz = camera_distance * Mathf.Cos(angle * Mathf.PI / 180);
+
+        camera.position = new Vector3(player.position.x - deltax,
+            player.position.y + camera_height,
+            player.position.z - deltaz);
+
+        Debug.Log("angle: " + angle + ",deltax:" + deltax + "deltaz: " + deltaz);
+       
+	}
+}
---
+++ Assets/script/player/FPSPlayer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSPlayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    private CharacterController m_ch;
+    public Transform m_transform;
+    private float m_moveSpeed = 6.0f;
+    private float m_jumpSpeed = 8.0f;
+
+    public int m_life = 5;
+    private float m_gravity = 20.0f;
+
+    private Transform m_camTransform;
+    private Vector3 m_camRot;        //camera rotation
+
+    private Vector3 movedirection=Vector3.zero;
+    
+    void Start()
+    {
+        m_transform = this.transform;
+        m_ch = this.GetComponent<CharacterController>();
+
+        m_camTransform = Camera.main.transform;
+
+        m_camTransform.rotation = m_transform.rotation;
+
+        // set camera eulerangles 
+        m_camRot = m_camTransform.eulerAngles;
+
+        Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_life <= 0) return;
+
+        // rotation camera
+        float rv = Input.GetAxis("Mouse Y");
+        float rh = Input.GetAxis("Mouse X");
+
+        m_camRot.x -= rv;
+        m_camRot.y += rh;
+
+        m_camTransform.eulerAngles = m_camRot;
+
+        // paler dynamically follow camera
+        Vector3 camrot = m_camTransform.eulerAngles;
+        camrot.x = 0; camrot.z = 0;     // only rotate on y axis
+        m_transform.eulerAngles = camrot;
+
+        if (m_ch.isGrounded)
+        {
+            movedirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movedirection = transform.TransformDirection(movedirection);
+            movedirection*=m_moveSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                movedirection.y = m_jumpSpeed;
+            }
+        }
+        movedirection.y -= m_gravity * Time.deltaTime;
+
+        m_ch.Move(movedirection*Time.deltaTime);
+    }
+    
+}
