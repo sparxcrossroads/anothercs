15ebb4af8971a3b237a23c067b15e784600f61e0
sparxcrossroads <kisshotblade@sina.com>
2015-03-20 12:00:35 +0800

    player状态机
    armypliot  animation clip 编辑 和 animator 调整编辑
    状态转换 代码转换

--- Assets/script/camera/camerafollow.cs
+++ Assets/script/camera/camerafollow.cs
@@ -2,17 +2,18 @@
 using System.Collections;
 
 public class camerafollow : MonoBehaviour {
-    public float camera_height = 2.0f;
-    public float camera_distance = 4.0f;
+    public float camera_height = 1.8f;
+    public float camera_distance = 1.0f;
 
     private Transform player;
     private Transform camera;
 
+    private Vector3 m_camrot;
 	// Use this for initialization
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("player").transform;
         camera = Camera.main.transform;
-
+        m_camrot = camera.eulerAngles;
 	}
 	
 	// Update is called once per frame
@@ -21,22 +22,29 @@ public class camerafollow : MonoBehaviour {
         //look at palyer
         //camera.LookAt(player);
 
-        //forward
-        camera.eulerAngles = new Vector3(camera.eulerAngles.x,
-            player.eulerAngles.y,
-            camera.eulerAngles.z);
+        float rv = Input.GetAxis("Mouse Y");
+        float rh = Input.GetAxis("Mouse X");
+        m_camrot.x -= rv;
+        m_camrot.y += rh;
+        camera.eulerAngles = m_camrot;
 
-        
+        //forward
+        //camera.eulerAngles = new Vector3(camera.eulerAngles.x,
+        //    player.eulerAngles.y,
+        //    camera.eulerAngles.z);
+
         float angle = camera.eulerAngles.y;
 
         float deltax = camera_distance * Mathf.Sin(angle * Mathf.PI / 180);
         float deltaz = camera_distance * Mathf.Cos(angle * Mathf.PI / 180);
 
-        camera.position = new Vector3(player.position.x - deltax,
+        
+        camera.position = new Vector3(player.position.x - deltax+0.5f,
             player.position.y + camera_height,
             player.position.z - deltaz);
 
-        Debug.Log("angle: " + angle + ",deltax:" + deltax + "deltaz: " + deltaz);
-       
+        //Debug.Log("angle: " + angle + ",deltax:" + deltax + "deltaz: " + deltaz);
+        //Debug.Log(player.position);
+        //Debug.Log(camera.position);
 	}
 }
--- Assets/script/player/FPSPlayer.cs
+++ Assets/script/player/FPSPlayer.cs
@@ -7,65 +7,153 @@ public class FPSPlayer : MonoBehaviour
     // Use this for initialization
     private CharacterController m_ch;
     public Transform m_transform;
-    private float m_moveSpeed = 6.0f;
-    private float m_jumpSpeed = 8.0f;
+    private Animator m_ani;
 
+    private float m_moveSpeed = 2.5f;
+    private float m_jumpSpeed = 8.0f;
+    private float m_rotaspeed = 100.0f;
     public int m_life = 5;
     private float m_gravity = 20.0f;
-
-    private Transform m_camTransform;
-    private Vector3 m_camRot;        //camera rotation
+    //0 walk 1 run
+    private int fire_WalkorRun = 0;
+    private float m_timer = 0;
 
     private Vector3 movedirection=Vector3.zero;
-    
     void Start()
     {
         m_transform = this.transform;
         m_ch = this.GetComponent<CharacterController>();
+        m_ani = this.GetComponent<Animator>();
 
-        m_camTransform = Camera.main.transform;
-
-        m_camTransform.rotation = m_transform.rotation;
-
-        // set camera eulerangles 
-        m_camRot = m_camTransform.eulerAngles;
-
-        Screen.lockCursor = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (m_life <= 0) return;
+        m_transform.eulerAngles=new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        AnimatorStateInfo stateinfo = m_ani.GetCurrentAnimatorStateInfo(0);
 
-        // rotation camera
-        float rv = Input.GetAxis("Mouse Y");
-        float rh = Input.GetAxis("Mouse X");
-
-        m_camRot.x -= rv;
-        m_camRot.y += rh;
-
-        m_camTransform.eulerAngles = m_camRot;
-
-        // paler dynamically follow camera
-        Vector3 camrot = m_camTransform.eulerAngles;
-        camrot.x = 0; camrot.z = 0;     // only rotate on y axis
-        m_transform.eulerAngles = camrot;
 
         if (m_ch.isGrounded)
         {
             movedirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             movedirection = transform.TransformDirection(movedirection);
-            movedirection*=m_moveSpeed;
+            movedirection *= m_moveSpeed;
 
             if (Input.GetButton("Jump"))
             {
                 movedirection.y = m_jumpSpeed;
             }
+
         }
+
         movedirection.y -= m_gravity * Time.deltaTime;
 
-        m_ch.Move(movedirection*Time.deltaTime);
+        m_ch.Move(movedirection * Time.deltaTime);
+
+        transform.Rotate(0, 0, 0);
+
+         // @animation
+         // idle
+        if (stateinfo.nameHash == Animator.StringToHash("Base Layer.idle") && !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("idle", false);
+
+            if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+                m_ani.SetBool("walk", true);
+
+            if (Input.GetButton("Jump"))
+                m_ani.SetBool("jump", true);
+
+            if (Input.GetButton("Fire1"))
+                m_ani.SetBool("idlefire", true);
+
+            if (Input.GetKey(KeyCode.R))
+            {
+                print("reload");
+                m_ani.SetBool("reload", true);
+            }
+        }
+
+        // walk
+        if (stateinfo.nameHash == Animator.StringToHash("Base Layer.walk") && !m_ani.IsInTransition(0))
+        {
+          
+            m_ani.SetBool("walk", false);
+
+            if (!Input.anyKey)
+                m_ani.SetBool("idle", true);
+
+            if (Input.GetButton("Jump"))
+                m_ani.SetBool("jump", true);
+
+            if (Input.GetButton("Fire1"))
+                m_ani.SetBool("walkfire", true);
+
+            if (Input.GetKey(KeyCode.R))
+                m_ani.SetBool("reload", true);
+
+        }
+
+        // jump
+        if (stateinfo.nameHash == Animator.StringToHash("Base Layer.jump") && !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("jump", false);
+
+            if (m_ch.isGrounded)
+                m_ani.SetBool("idle", true);
+
+        }
+
+        // idlefire
+        if (stateinfo.nameHash == Animator.StringToHash("Base Layer.idlefire") && !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("idlefire", false);
+
+            //print("idlefire");
+            m_timer += Time.deltaTime;
+            print(m_timer.ToString());
+
+            if (m_timer > 0.8)
+            {
+                print("/rsssss");
+                m_ani.SetBool("idle", true);
+            }
+
+            if (Input.GetKey(KeyCode.R))
+            {
+                print("reload");
+                m_ani.SetBool("reload", true);
+            }
+        }
+
+        // walkfire
+        if (stateinfo.nameHash == Animator.StringToHash("Base Layer.walkfire") && !m_ani.IsInTransition(0))
+        {
+            m_ani.SetBool("walkfire", false);
+
+            if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+                m_ani.SetBool("walk", true);
+
+            if (Input.GetKey(KeyCode.R))
+                m_ani.SetBool("reload", true);
+
+
+        }
+
+        //reload
+         if (stateinfo.nameHash == Animator.StringToHash("Base Layer.reload") && !m_ani.IsInTransition(0))
+         {
+             m_ani.SetBool("reload", false);
+             //finsh
+             if (stateinfo.normalizedTime > 1.0f)
+                 m_ani.SetBool("idle", true);
+
+         }
+
+         if (m_timer >0.8)
+             m_timer = 0;
     }
     
 }
